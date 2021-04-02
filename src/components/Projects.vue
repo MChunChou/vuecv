50e6ca8b1deaba3670d59da84cf5c8140af4f640
MChunChou <mchunchou89@gmail.com>
2021-04-02 18:10:52 +0800

    Add Projects page , and modified skills

---
+++ src/components/Projects.vue
@@ -0,0 +1,173 @@
+<template>
+  <section class="projects" id="projects">
+    <div class="title">
+      <h2>My Projects</h2>
+      <p></p>
+    </div>
+    <div class="content">
+      <div class="projectBx">
+        <div class="name">
+          <img src="../assets/projects/project1.jpg" />
+          <h2>檔案上傳介面</h2>
+          <p></p>
+        </div>
+        <div class="describe">
+          <pre>
+使用 ReactJS 開發提供上傳檔案的介面 
+          Note: 連結網頁沒有 server 支援, 使用 JS 模擬 servere</pre
+          >
+          <a href="https://mchunchou.github.io/simulateUpload/" target="_blank"
+            >Go ></a
+          >
+        </div>
+      </div>
+      <div class="projectBx">
+        <div class="name">
+          <img src="../assets/projects/project2.jpg" />
+          <h2>gmcode 模擬</h2>
+          <p></p>
+        </div>
+        <div class="describe">
+          <pre>輸入 GMcode 並以此產生路徑模擬 </pre>
+          <a href="https://mchunchou.github.io/NcEmulator/" target="_blank"
+            >Go ></a
+          >
+        </div>
+      </div>
+      <div class="projectBx">
+        <div class="name">
+          <img src="../assets/projects/project3.jpg" />
+          <h2>古蹟資料列表</h2>
+          <p></p>
+        </div>
+        <div class="describe">
+          <pre>嘗試使用 React Native 抓取 API 並加以顯示</pre>
+          <!-- <a href="https://github.com/MChunChou" target="_blank">Go ></a> -->
+        </div>
+      </div>
+      <div class="projectBx">
+        <div class="name">
+          <img src="../assets/projects/project4.jpg" />
+          <h2>鋼琴樂譜辨識及播放系統</h2>
+          <p></p>
+        </div>
+        <div class="describe">
+          <pre>
+使用 JAVA 開發對於鋼琴樂譜的影像辨識系統
+透過分析音符的位置判斷在使用 midi 播放</pre
+          >
+          <!-- <a href="https://github.com/MChunChou" target="_blank">Go ></a> -->
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+  setup() {
+    const projectsData = [
+      {
+        name: "",
+        des: ``,
+        url: "",
+      },
+      {
+        name: "",
+        des: ``,
+        url: "",
+      },
+    ];
+    console.log(projectsData)
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+$old-rose: rgba(170, 118, 124, 1);
+$paradise-pink: rgba(239, 71, 111, 1);
+$onyx: rgba(53, 57, 60, 1);
+$linen: rgb(243, 241, 235);
+$cerulean-frost: rgba(89, 152, 197, 1);
+
+.projects {
+  .content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: wrap;
+
+    .projectBx {
+      padding: 40px 20px;
+      margin: 20px;
+      text-align: center;
+      background: $linen;
+      transition: 0.5s;
+      width: 20%;
+      min-width: 350px;
+
+      .name {
+        img {
+          position: relative;
+          max-width: 300px;
+          max-height: 300px;
+          transition: 0.5s;
+          object-fit: cover;
+        }
+
+        h2 {
+          margin-top: 20px;
+          margin-bottom: 10px;
+          font-size: 20px;
+          font-weight: 600;
+        }
+
+        p {
+        }
+      }
+
+      .describe {
+        display: grid;
+        grid-template-rows: 3fr 1fr;
+        position: relative;
+        padding: 0px 20px;
+        left: -50%;
+        transition: 0.5s;
+        align-items: center;
+        p,
+        pre,
+        a {
+          display: none;
+        }
+
+        a {
+          text-decoration: none;
+        }
+      }
+
+      &:hover {
+        display: grid;
+        width: 100%;
+        background: black;
+        grid-template-columns: 1fr 2fr;
+        transition: 0.5s;
+        transition-property: width;
+        color: $linen;
+
+        .describe {
+          left: 0;
+
+          p,
+          pre,
+          a {
+            display: block;
+            color: $linen;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,24 +1,310 @@
 <template>
-    <section class="skills" id="skills">
-        <div class="title">
-            <h2>Skills</h2>
-            <p></p>
-        </div>
-        <div class="content">
-            <div class="skillsBx">
-                <h2>FrontEnd</h2>                
-            </div>
-            <div class="skillsBx">
-                <h2>BackEnd</h2>
-            </div>
-        </div>
-    </section>
+  <section class="skills" id="skills">
+    <div class="title">
+      <h2>Skills</h2>
+      <p></p>
+    </div>
+    <div class="content">
+      <div class="skillBx">
+        <h2>FrontEnd</h2>
+        <p class="d3Ctn"></p>
+        <p class="card">          
+          <div class="cardBx" v-for="(obj, key) in cards.front" :key="key">
+            <h3>{{ obj.title }}:</h3>
+            <ul>
+              <li v-for="(name) in obj.include" :key="name"> {{ name }} </li>
+            </ul>
+          </div>
+        </p>
+      </div>
+      <div class="skillBx">
+        <h2>BackEnd</h2>
+        <p class="d3Ctn"></p>
+        <p class="card">
+          <div class="cardBx" v-for="(obj, key) in cards.back" :key="key">
+            <h3>{{ obj.title }}:</h3>
+            <ul>
+              <li v-for="(name) in obj.include" :key="name"> {{ name }} </li>
+            </ul>
+          </div>
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
-export default {
-    name: "Skills"
+import { onMounted, onUnmounted } from "vue";
+import * as d3 from "d3";
+
+function createD3SVG(data, winWidth) {
+  let { width, height } = d3.select(".d3Ctn").node().getBoundingClientRect();
+  const fontSize = winWidth <= 991 ? 15 : 23;
+
+  const pie = d3
+    .pie()
+    .sort(null)
+    .value((d) => d.value);
+
+  const radius = (Math.min(width, height) / 2) * 0.8;
+
+  const arcLabel = d3.arc().innerRadius(radius).outerRadius(radius);
+
+  const arc = d3
+    .arc()
+    .innerRadius(0)
+    .outerRadius(Math.min(width, height) / 2 - 1);
+
+  const color = d3
+    .scaleOrdinal()
+    .domain(data.map((d) => d.name))
+    .range(
+      d3
+        .quantize((t) => d3.interpolateSpectral(t * 0.8 + 0.1), data.length)
+        .reverse()
+    );
+
+  const arcs = pie(data);
+
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+  svg
+    .append("g")
+    .attr("stroke", "white")
+    .selectAll("path")
+    .data(arcs)
+    .join("path")
+    .attr("fill", (d) => color(d.data.name))
+    .attr("d", arc)
+    .append("title")
+    .text((d) => `${d.data.name}: ${d.data.value}`);
+
+  svg
+    .append("g")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", fontSize)
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(arcs)
+    .join("text")
+    .attr("transform", (d) => `translate(${arcLabel.centroid(d)})`)
+    .call((text) =>
+      text
+        .append("tspan")
+        .attr("y", "-0.4em")
+        .attr("font-weight", "bold")
+        .text((d) => d.data.name)
+    )
+    .call((text) =>
+      text
+        .filter((d) => d.endAngle - d.startAngle > 0.25)
+        .append("tspan")
+        .attr("x", 0)
+        .attr("y", "0.7em")
+        .attr("fill-opacity", 0.7)
+        .text((d) => d.data.value.toLocaleString() + "%")
+    );
+
+  return svg.node();
 }
+
+export default {
+  name: "Skills",
+  setup() {
+    const cards = {
+      front: [
+        {
+          title: "編譯",
+          include: ["webpack", "babel"],
+        },
+        {
+          title: "Language",
+          include: ["Html", "Javascript", "ReactJs", "VueJS", "Css"],
+        },
+        {
+          title: "Other",
+          include: ["WebSocket", "GraphQL"],
+        },
+      ],
+      back: [
+        {
+          title: "Language",
+          include: ["NodeJS", "Rust", "Java"],
+        },
+        {
+          title: "Database",
+          include: ["Postgresql"],
+        },
+      ],
+    };
+
+    const d3Data = {
+      front: [
+        { name: "Html", value: 10 },
+        { name: "Css", value: 15 },
+        { name: "Javascript", value: 20 },
+        { name: "ReactJs", value: 25 },
+        { name: "VueJs", value: 10 },
+        { name: "Webpack", value: 5 },
+        { name: "Babel", value: 5 },
+        { name: "WebSocket", value: 5 },
+        { name: "GraphQL", value: 5 },
+      ],
+      back: [
+        { name: "Java", value: 15 },
+        { name: "Rust", value: 30 },
+        { name: "NodeJs", value: 40 },
+        { name: "Postgresql", value: 15 },
+      ],
+    };
+
+    function renderD3(winWidth) {
+      d3.selectAll(".d3Ctn svg").remove();
+      const front = createD3SVG(d3Data.front, winWidth);
+      const back = createD3SVG(d3Data.back, winWidth);
+      d3.selectAll(".d3Ctn").nodes()[0].append(front);
+      d3.selectAll(".d3Ctn").nodes()[1].append(back);
+    }
+
+    onMounted(() => {
+      window.addEventListener("resize", (e) => {
+        renderD3(e.target.innerWidth);
+      });
+
+      renderD3(window.innerWidth);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", () => {});
+    });
+    return {
+      cards,
+    };
+  },
+};
 </script>
 <style lang="scss">
-    
+.skills {
+  .content {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    height: 100%;
+    width: 89%;
+
+    .skillBx {
+      position: relative;
+      padding: 40px 20px;
+      height: 100%;
+      width: 50%;
+      text-align: center;
+
+      .d3Ctn {
+        padding: 20px;
+        position: absolute;
+        height: 80%;
+        width: 100%;
+      }
+
+      p {
+        margin: 0;
+      }
+
+      .card {
+        position: absolute;
+        padding: 20px;
+        height: 80%;
+        width: 100%;
+        background: white;
+        
+        &:hover {
+          opacity: 0;
+          transition: 0.5s;
+
+        }
+
+        h3 {
+          padding: 10px;
+          text-align: center;
+          width: 176px;
+          border-right: 2px solid white;
+        }
+
+        .cardBx {
+          display: flex;
+          flex-direction: row;
+          background: #eee;
+          margin: 5px;
+          position: relative;
+          min-height: 25%;
+          border-radius: 20px;
+          ul {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: center;
+            width: 100%;
+            li {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              list-style-type: none;
+              padding: 20px 10px;
+              border: solid 2px #111;
+              border-radius: 20px;
+              margin: 7px 10px;
+              width: 150px;
+              height: 50px;
+              font-size: 1.1em;
+              font-weight: bold;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 991px) {
+  .skills {
+    min-height: 110vh;
+
+    .content {
+      justify-content: flex-start;
+      flex-direction: column;
+      align-items: center;
+      .skillBx {
+        height: 49%;
+        width: 100%;
+
+        .d3Ctn {
+          padding: 20px;
+          height: 90%;
+          width: 100%;
+        }
+
+        .card {
+          height: 95%;
+          width: 100%;
+          padding: 0;          
+          .cardBx {
+            ul {              
+              li {
+                margin: 3px;
+                padding: 0px;
+                font-size: 0.9em;
+                height: 30px;
+                width: 100px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
+
